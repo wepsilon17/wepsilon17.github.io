4bb1dc8eaf2a0a01eda87204b3f65402269bb578
wepsilon17 <wallace.peaslee+github@gmail.com>
2025-11-02 17:50:09 +0000

    Deploying to gh-pages from @ wepsilon17/wepsilon17.github.io@16d0f99d7d2c1b27051476feb5b7e2e57ce6e86d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,19 +48,19 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-music-manuscript-reconstruction",
+          title: 'Music Manuscript Reconstruction',
+          description: "Recovering notation from degraded music manuscripts.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-arch",
+          title: 'ArCH',
+          description: "AI for Cultural Heritage Hub",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{
