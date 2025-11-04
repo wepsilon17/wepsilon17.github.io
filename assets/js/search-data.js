be4ea1003908ee4a0a6485e409095ae6230c39d2
wepsilon17 <wallace.peaslee+github@gmail.com>
2025-11-04 12:49:15 +0000

    Deploying to gh-pages from @ wepsilon17/wepsilon17.github.io@19aa424721e64d55689e9ed4a2ed2df953f17e1d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -68,9 +68,9 @@ ninja.data = [{
           description: "Domain Generalization for Punch Mark Classification",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-punch-mark-classification",
-          title: 'Punch Mark Classification',
-          description: "Forbidden Configurations",
+            },},{id: "projects-forbidden-configurations",
+          title: 'Forbidden Configurations',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{
